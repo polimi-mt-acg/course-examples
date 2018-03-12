732bfeee791098fc3e9ca0ad258e7b5a1f7d4bfe
Leonardo Arcari <lippol94@gmail.com>
2018-03-12 19:58:42 +0100

    First working implementation of JPA example
    Signed-off-by: Leonardo Arcari <lippol94@gmail.com>

--- JPA/src/main/java/com/github/polimi_mt_acg/User.java
+++ JPA/src/main/java/com/github/polimi_mt_acg/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
